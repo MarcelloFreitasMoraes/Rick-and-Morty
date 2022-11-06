366d6c44727fae5d16e901f9d40fed43a94883b8
Marcelo Moraes <mamoressp@gmail.com>
2022-11-06 10:05:37 -0300

    finalizado

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,12 +5,12 @@ export default function Cards({ data }: CardProps) {
 
     return (
         <div className={styles.container}>
+                <h2>{data?.name}</h2>
              {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={data.image} className={styles.image} alt={'Personagens'} />
-                <h2>{data.name}</h2>
+            <img src={data?.image} className={styles.image} alt={'Personagens'} />
+                <p>SPECIES: {data?.species}</p>
                 <p>STATUS: {data?.status}</p>
                 <p>GENDER: {data?.gender}</p>
-                <p>SPECIES: {data?.species}</p>
         </div>
     )
 }
